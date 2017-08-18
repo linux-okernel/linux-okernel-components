85044b8f57bbf7c5983e7f6beefdcf1217607d54
Nigel Edwards <nigel.edwards@hpe.com>
2017-08-18 10:29:19 +0100

    First working version: patches linux_proc_banner. Much tidying and further development needed.

--- test_mappings/kwriter/kwriter.c
+++ test_mappings/kwriter/kwriter.c
@@ -33,25 +33,29 @@ if not, write to:
  * Attempt to write kernel memory to demonstrate okernel protection
  */
 void oktargets(unsigned long (*address)[]);
+void okcheck_va(unsigned long va);
+void okset_mem_rw(unsigned long va);
 #define BUFFMAX 1000
-#define PG_BIT 10
+#define PG_BIT 20
 
 static unsigned long mod_start = (unsigned long)PFN_ALIGN(MODULES_VADDR);
 static unsigned long mod_end = (unsigned long)PFN_ALIGN(MODULES_END);
-static unsigned long targets[8];
+static unsigned long targets[10];
 static unsigned long kw_text;
 static unsigned long kw_etext;
 static unsigned long kw__start_rodata;
 static unsigned long kw__end_rodata;
 static int (*kw_set_memory_rw)(unsigned long addr, int numpages);
+static void (*kw_flush_tlb_all)(void);
 static void *(*kw_text_poke)(void *addr, const void *opcode, size_t len);
+static void (*kw_flush_tlb_kernel_range)(unsigned long start, unsigned long end);
 struct mutex *kw_text_mutex;
-static char *kw_linux_banner;
+static char *kw_linux_proc_banner;
 static char old_banner[BUFFMAX];
 
-static const char *patched_banner = "Successfully patched linux_banner";
+static const char *patched_banner = "Successfully patched linux_proc_banner";
 static const unsigned long pgmask = ~(((unsigned long)1 << PG_BIT) - 1);
-
+static const unsigned long psize = ((unsigned long)1 << PG_BIT);
 static void __init get_targets(void)
 {
 	printk("kwriter pgmask %#lx\n", pgmask);
@@ -63,9 +67,12 @@ static void __init get_targets(void)
 	kw__start_rodata = targets[2];
 	kw__end_rodata = targets[3];
 	kw_set_memory_rw = (int (*)(unsigned long, int))targets[4];
-	kw_linux_banner = (char *)targets[5];
-	kw_text_poke = (void *(*)(void *, const void *, size_t))targets[6];
-	text_mutex = (struct mutex *)targets[7];
+	kw_flush_tlb_all =  (void (*)(void))targets[5];
+	kw_linux_proc_banner = (char *)targets[6];
+	kw_text_poke = (void *(*)(void *, const void *, size_t))targets[7];
+	kw_text_mutex = (struct mutex *)targets[8];
+	kw_flush_tlb_kernel_range =
+		(void (*)(unsigned long, unsigned long))targets[9];
 
 	printk(KERN_INFO "kwriter _text %#lx\n", kw_text);
 	printk(KERN_INFO "kwriter _etext %#lx\n", kw_etext);
@@ -73,47 +80,74 @@ static void __init get_targets(void)
 	printk(KERN_INFO "kwriter __end_rodata va %#lx\n", kw__end_rodata);
 	printk(KERN_INFO "kwriter set_memory_rw %#lx\n",
 	       (unsigned long) kw_set_memory_rw);
-	printk(KERN_INFO "kwriter linux_banner %s\n", kw_linux_banner);
+	printk(KERN_INFO "kwriter linux_proc_banner %s\n", kw_linux_proc_banner);
+	printk(KERN_INFO "kwriter linux_proc_banner va %#lx\n",
+	       (unsigned long) kw_linux_proc_banner);
 	printk(KERN_INFO "kwriter module space start va %#lx\n", mod_start);
 	printk(KERN_INFO "kwriter module space end va %#lx\n", mod_end);
+	printk(KERN_INFO "kwriter flush_tlb_kernel_range %#lx\n",
+	       (unsigned long) kw_flush_tlb_kernel_range);
 }
  static void old_update_banner(void)
  {
  	size_t n;
 	printk(KERN_INFO "kwriter attempting to get text_mutex\n");
-	if (!mutex_trylock(text_mutex)) {
+	if (!mutex_trylock(kw_text_mutex)) {
 		printk(KERN_INFO "kwriter Unable to get text_mutex\n");
  	}
 	printk(KERN_INFO "kwriter Got text_mutex\n");
- 	if (!(strncpy(old_banner, kw_linux_banner, BUFFMAX))) {
+ 	if (!(strncpy(old_banner, kw_linux_proc_banner, BUFFMAX))) {
 		printk(KERN_INFO "kwriter strncpy failed\n");
 		goto end;
  	}
- 	printk(KERN_INFO "kwriter attempting to patch linux_banner");
+ 	printk(KERN_INFO "kwriter attempting to patch linux_proc_banner");
 	n = strlen(patched_banner);
-//	kw_text_poke((void *)kw_linux_banner, (void *)patched_banner, n);
+//	kw_text_poke((void *)kw_linux_proc_banner, (void *)patched_banner, n);
 end:
-	mutex_unlock(text_mutex);
+	mutex_unlock(kw_text_mutex);
  }
 
 static void update_banner(void)
 {
 	size_t n;
-	printk(KERN_INFO "kwriter attempting to set page rw\n");
-	if (kw_set_memory_rw(((unsigned long)kw_linux_banner) && pgmask, 1)) {
+	unsigned long va = ((unsigned long)kw_linux_proc_banner) & pgmask;
+	printk(KERN_INFO "kwriter attempting to set page rw %#lx\n", va);
+	if (kw_set_memory_rw(va, 1)) {
 		printk(KERN_INFO "kwriter kw_set_memory_rw failed\n");
 		return;
 	}
-	if (!(strncpy(old_banner, kw_linux_banner, BUFFMAX))) {
+	kw_flush_tlb_all();
+	kw_flush_tlb_kernel_range(va, va + psize);
+	printk(KERN_INFO "kwriter flushed tlb and page\n");
+	if (!(strncpy(old_banner, kw_linux_proc_banner, BUFFMAX))) {
 		printk(KERN_INFO "kwriter failed to save banner\n");
 		return;
 	}
-	printk(KERN_INFO "kwriter attempting to patch linux_banner");
-	n = strlen(kw_linux_banner + 1);
-	if (!(strncpy(kw_linux_banner, patched_banner, n))) {
+	okcheck_va((unsigned long)kw_linux_proc_banner);
+	okcheck_va(va);
+	/* Check that we can still read kw_linux_proc_banner get the
+	 * protections from the page table and check they are what we
+	 * expect them to be. Insert memory barriers?
+	 */
+	printk(KERN_INFO "Calling okset_mem_rw");
+	okset_mem_rw(va);
+	okcheck_va(va);
+	kw_flush_tlb_all();
+	kw_flush_tlb_kernel_range(va, va + psize);
+	printk(KERN_INFO "kwriter flushed tlb and page\n");
+	printk(KERN_INFO "Checking we can still read linux_proc_banner %s\n",
+	       kw_linux_proc_banner);
+	n = strlen(kw_linux_proc_banner + 1);
+	printk(KERN_INFO "kwriter patching linux_proc_banner len is %ld bytes\n", n);
+	if (n > strlen(patched_banner))
+		n = strlen(patched_banner) + 1;
+	printk(KERN_INFO "kwriter patching linux_proc_banner with %ld bytes\n", n);
+	if (!(strncpy(kw_linux_proc_banner, patched_banner, n))) {
  		printk(KERN_INFO "kwriter patching banner failed\n");
 		return;
 	}
+	okcheck_va((unsigned long)kw_linux_proc_banner);
+	printk(KERN_INFO "kwriter linux_proc_banner %s\n", kw_linux_proc_banner);
 }
 
 static int __init kwriter_module_init(void)
